78712de13729b825fb5a600a0a87ea807c5b4a5b
alumno <alumno@utnfra.com.ar>
2020-02-10 19:15:12 -0300

    tp1 terminado

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,31 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var precioUno  = parseInt(document.getElementById("PrecioUno").value);
+    var precioDos  = parseInt(document.getElementById("PrecioDos").value);
+    var precioTres = parseInt(document.getElementById("PrecioTres").value);
+
+    alert(precioUno + precioDos + precioTres);
 }
+
+
 function Promedio () 
 {
-	
+    var precioUno  = parseInt(document.getElementById("PrecioUno").value);
+    var precioDos  = parseInt(document.getElementById("PrecioDos").value);
+    var precioTres = parseInt(document.getElementById("PrecioTres").value);
+    
+    alert((precioUno + precioDos + precioTres) / 3);
 }
+
+
 function PrecioFinal () 
 {
-	
+	var precioUno  = parseInt(document.getElementById("PrecioUno").value);
+    var precioDos  = parseInt(document.getElementById("PrecioDos").value);
+    var precioTres = parseInt(document.getElementById("PrecioTres").value);
+    var subtotal   = precioUno + precioDos + precioTres;
+    var iva        = 21;
+
+    alert((subtotal) + (subtotal * iva/100));
 }
