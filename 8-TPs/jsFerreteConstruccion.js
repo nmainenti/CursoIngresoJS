65fcdd7a2e900cede334b6e6446d030ed1e862e6
alumno <alumno@utnfra.com.ar>
2020-02-11 22:11:35 -0300

    tp2 terminado

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -4,15 +4,31 @@ B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
-function Rectangulo () 
+function Rectangulo ()
 {
+  var largo = parseInt(document.getElementById("Largo").value);
+  var ancho = parseInt(document.getElementById("Ancho").value);
 
+  var perimetro = ((largo + ancho) * 2);
+  alert("se necesitan " + perimetro * 3 + " metros de alambre");
 }
-function Circulo () 
+
+function Circulo ()
 {
-	
+  var radio = parseInt(document.getElementById("Radio").value);
+
+  var perimetro = 6.28 * radio;
+  alert("se necesitan " + perimetro + " metros de alambre");
 }
-function Materiales () 
+
+function Materiales ()
 {
-	
+  var largo = parseInt(document.getElementById("Largo").value);
+  var ancho = parseInt(document.getElementById("Ancho").value);
+  var area  = largo * ancho;
+
+  var cemento = area * 2;  // se necesitan 2 bolsas por m^2
+  var cal     = area * 3;  // se necesitan 3 bolsas por m^2
+
+  alert("se necesitan " + cemento + " bolsas de cemento y " + cal + " bolsas de cal");
 }
