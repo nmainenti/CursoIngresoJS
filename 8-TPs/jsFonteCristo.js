7b384cb0baa2c9ef6cc0f3d274f9be612bf8b3a7
alumno <alumno@utnfra.com.ar>
2020-02-18 22:09:19 -0300

    tp 8 terminado

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -7,7 +7,60 @@ c.	Se pedirán un número positivo y se mostrará la cantidad de números divisi
 d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
 e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
-function ComenzarIngreso () 
-{
- 	
+
+do var numero = parseInt(document.getElementById("numero").value);
+while(numero <= 0);
+
+function NumerosPares(numero){
+  var cantPar = 0;
+  for(var i = 1; i <= numero; i++){
+    if((i % 2) == 0){
+      cantPar++;
+    }
+  }
+  alert("hay " + cantPar + " números pares desde 1 hasta " + numero);
+}
+
+function NumerosImpares(numero){
+  var cantImpar = 0;
+  for(var i = 1; i <= numero; i++){
+    if((i % 2) == 1){
+      cantImpar++;
+    }
+  }
+  alert("hay " + cantImpar + " números pares desde 1 hasta " + numero);
+}
+
+function NumerosDivisibles(numero){
+  var cantDiv
+  for(var i = 1; i <= numero; i++){
+    if((i % numero) == 0){
+      cantDiv++;
+    }
+  }
+  alert("hay " + cantDiv + " números divisibles por " + numero);
+}
+
+function VerificarPrimo(numero){
+  var cantDiv
+  for(var i = 1; i <= numero; i++){
+    if((i % numero) == 0){
+      cantDiv++;
+    }
+  }
+  if(cantDiv == 2){
+    alert(numero + " es un número primo");
+  }
+}
+
+function NumerosPrimos(numero){
+  cantPrimos = 0;
+  for(var i = 1; i < numero; i++){
+    for(var x = 1; x <= i; x++){
+      if((i % x) == 0) cantDiv++;
+      if(cantDiv > 2) break;
+    }
+    if (cantDiv == 2) cantPrimos++;
+  }
+  alert("hay " + cantPrimos + " entre 1 y " + numero);
 }
--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,17 +9,56 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso() {
   do var edad   = parseInt(prompt("Ingresar edad (18 a 90)"));
-  while(edad < 18 || edad > 90);
+  while(edad < 18 || edad > 90 || isNaN(edad));
   do var sexo   = (prompt("Ingresar sexo (M = Masculino , F = Femenino")).toLowerCase();
   while(sexo != "f" && sexo != "m");
   do var ecivil = parseInt(prompt("Ingresar estado civil (1 = Soltero , 2 = Casado , 3 = Divorciado , 4 = Viudo"));
-  while(ecivil < 1 || ecivil > 4);
+  while(ecivil < 1 || ecivil > 4 || isNaN(ecivil));
   do var sueldo = parseInt(prompt("Ingresar sueldo bruto (mayor o igual a 8000)"));
-  while(sueldo < 8000);
+  while(sueldo < 8000 || isNaN(sueldo));
   do var legajo = parseInt(prompt("Ingresar número de legajo, solo números, cuatro cifras, sin ceros a la izquierda"));
-  while(legajo > 9999);
+  while(legajo > 9999 || isNaN(legajo));
   do var nacionalidad = (prompt("Ingresar nacionalidad (A = Argentino , E = Extranjero , N = Nacionalizado")).toLowerCase();
   while(nacionalidad != "a" && nacionalidad != "e" && nacionalidad != "n");
 
+  switch(sexo){
+    case 'f':
+      sexo = "Femenino";
+      break;
+    case 'm':
+      sexo = "Masculino";
+      break;
+  }
+  switch(ecivil){
+    case 1:
+      ecivil = "Soltero";
+      break;
+    case 2:
+      ecivil = "Casado";
+      break;
+    case 3:
+      ecivil = "Divorciado";
+      break;
+    case 4:
+      ecivil = "Viudo";
+      break;
+  }
+  switch(nacionalidad){
+    case 'a':
+      nacionalidad = "Argentino";
+      break;
+    case 'e':
+      nacionalidad = "Extranjero";
+      break;
+    case 'n':
+      nacionalidad = "Nacionalizado";
+      break;
+  }
 
+  document.getElementById("Edad").value = edad;
+  document.getElementById("Sexo").value = sexo;
+  document.getElementById("EstadoCivil").value = ecivil;
+  document.getElementById("Sueldo").value = sueldo;
+  document.getElementById("Legajo").value = legajo;
+  document.getElementById("Nacionalidad").value = nacionalidad;
 }
